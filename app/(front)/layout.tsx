590c7d1bbf2d712993bb576dc89ce8636f1e317c
sanaevvv <sanaevvv@gmail.com>
2024-05-03 15:00:46 +0900

    added navbar and hero sections

---
+++ app/(front)/layout.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/Frontend/Navbar";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+      <Navbar />
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function Home() {
-  return (
-    <section className="text-3xl flex min-h-screen justify-center items-center">
-      <h2 className="font-semibold">Medical App</h2>
-    </section>
-  );
-}
